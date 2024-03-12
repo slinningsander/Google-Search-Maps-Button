c54fb64fba7579d1961b79fa605fb2fbdec336e9
Sander Garland Slinning <slinningsander@gmail.com>
2024-03-12 19:46:55 +0100

    Removed boilerplate code and unnecessary references to Google

--- Google Search Maps Button Extension/Resources/content.js
+++ Google Search Maps Button Extension/Resources/content.js
@@ -1,13 +1,3 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
-
-
-
 function addMapsButton(currentSearchTerm) {
     let targetDiv = document.querySelector('.IUOThf');
     if (targetDiv && !document.querySelector('.mapsButton')) {
--- Google Search Maps Button/Resources/Script.js
+++ Google Search Maps Button/Resources/Script.js
@@ -1,8 +1,8 @@
 function show(enabled, useSettingsInsteadOfPreferences) {
     if (useSettingsInsteadOfPreferences) {
-        document.getElementsByClassName('state-on')[0].innerText = "Google Search Maps Button’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('state-off')[0].innerText = "Google Search Maps Button’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('state-unknown')[0].innerText = "You can turn on Google Search Maps Button’s extension in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('state-on')[0].innerText = "Search Maps Button’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('state-off')[0].innerText = "Search Maps Button’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('state-unknown')[0].innerText = "You can turn on Search Maps Button’s extension in the Extensions section of Safari Settings.";
         document.getElementsByClassName('open-preferences')[0].innerText = "Quit and Open Safari Settings…";
     }
 
